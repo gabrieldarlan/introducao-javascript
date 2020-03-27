2e11194dbccc90e13fb0af78478d2fb2002d8093
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-27 20:11:16 -0300

    aula 02 - atividade 07

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,36 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Thalyta Nutricionista";
 
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+const altura = tdAltura.textContent;
+
+const tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhvalida = true;
+
+
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("altura inválida");
+    alturaEhvalida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhvalida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
